ed137ff4c91ae515ab9b6355497a8358b7d3070b
Thomas Hart <t.j.hart89@hotmail.co.uk>
2022-11-14 14:16:42 +0000

    Finish category search

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,28 +4,40 @@ const db = require("../db");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
 
-
 beforeEach(() => seed(testData));
 
 afterAll(() => {
   if (db.end) db.end();
 });
 
-describe('GET /api/categories', () => {
-    test('status - 200, an array of category objects', () => {
-        return request(app)
-            .get('/api/categories')
-            .expect(200)
-            .then((body) => {
-                expect(body.categories).toBeInstanceOf(Array);
-                body.categories.forEach((category) => {
-                    expect(category).toEqual(
-                        expect.objectContaining({
-                            slug: expect.any(String),
-                            description: expect.any(String),
-                        }))
-                })
-                
-        })
-    })
-})
+describe("GET /api/categories", () => {
+  test("status - 200, an array of category objects", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        console.log(categories);
+        expect(categories).toBeInstanceOf(Array);
+        body.categories.forEach((category) => {
+          expect(category).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
+
+describe("ERROR / typo or unknown path", () => {
+  test("status - 404, error with message route not found", () => {
+    return request(app)
+      .get("/api/skdjhfks")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Route not found");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const { getCategories } = require("./controller");
 
 const app = express();
 app.use(express.json());
+
+app.get("/api/categories", getCategories);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Route not found" });
+});
 
 module.exports = app;
