b1a6a839f93c4402a76552541605687d1a93ee18
Thomas Hart <t.j.hart89@hotmail.co.uk>
2022-11-15 14:27:25 +0000

    Finish review by review id

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const { getCategories, getReviews } = require("./controller");
+const {
+  getCategories,
+  getReviews,
+  getReviewByReviewId,
+} = require("./controller");
 
 const app = express();
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id", getReviewByReviewId);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid ID" });
+  } else res.status(err.status).send({ msg: err.msg });
 });
 
 module.exports = app;
