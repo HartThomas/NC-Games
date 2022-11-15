706697e4dc857935bcc10894c1d1b6390934f3da
Thomas Hart <t.j.hart89@hotmail.co.uk>
2022-11-15 14:42:11 +0000

    Make minor changes to the tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -86,7 +86,7 @@ describe("GET /api/reviews/:review_id", () => {
           expect.objectContaining({
             owner: expect.any(String),
             title: expect.any(String),
-            review_id: expect.any(Number),
+            review_id: 4,
             category: expect.any(String),
             review_img_url: expect.any(String),
             created_at: expect.any(String),
